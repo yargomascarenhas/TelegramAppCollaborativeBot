652a9402d676c2a64aac3013a27b0e0871304cbc
Yargo <yargomascarenhas@gmail.com>
2021-10-05 18:43:49 -0300

    todo

--- src/bot.js
+++ src/bot.js
@@ -33,7 +33,7 @@ module.exports = class Bot {
 
     static sendMessageMarkdown(chat_id, message) {
         return new Promise(function(resolve, reject) {
-            console.log('STARTING sendMessageHtml');
+            console.log('STARTING sendMessageMarkdown');
             console.log(message);
             console.log(chat_id);
             Api.postByBody(
@@ -118,37 +118,251 @@ module.exports = class Bot {
         Collaborative.getUserByChatId(req.body.message.chat.id)
         .then(function(datares) {
             if(datares.data && datares.data.id) {
-                Bot.sendMessage(
-                    req.body.message.chat.id,
-                    `Olá ${datares.data.data.first_name}, tudo bem?`
-                ).then(function(result) {
-                    console.log('DEU BOM');
-                    console.log(result);
-                    res.status(200).send({});
-                })
-                .catch(function(err) {
-                    console.log('DEU RUIM');
-                    console.log(err);
-                });
+                Bot.processConversation(req, datares)
+                .then(function() {res.status(200).send({})})
+                .catch(function() {res.status(200).send({})});
             } else {
                 Bot.sendMessage(
                     req.body.message.chat.id,
                     'Olá, eu ainda não te conheço, por favor digite a sua chave do AppCollaborative.'
-                ).then(function(result) {
-                    console.log('DEU BOM');
-                    console.log(result);
-                    res.status(200).send({});
-                })
-                .catch(function(err) {
-                    console.log('DEU RUIM');
-                    console.log(err);
-                })
+                ).then(function() {res.status(200).send({})})
+                .catch(function() {res.status(200).send({})});
             }
         })
         .catch(function(err) {
+            console.log('ERR', err);
             Bot.sendServerFailMessage(req)
             .then(function(result) {res.status(200).send({})})
             .catch(function(result) {res.status(200).send({})});
         });
+    }
+
+    static msgContains(message, text) {
+        return (message.toUpperCase().indexOf(text) !== -1);
+    }
+
+    static isDefault(datares) {
+        console.log('ISDEFAULT, DATARES', datares);
+        return datares.data.user.type === 'USER';
+    }
+
+    static isPartner(datares) {
+        return datares.data.user.type === 'PARTNER';
+    }
+
+    static respostaDireta(messagetext) {
+        console.log('respostaDireta');
+        let response = '';
+        if(Bot.msgContains(messagetext, 'OI')) {
+            response = `Oi, como posso ajudar?`;
+        }
+        if(Bot.msgContains(messagetext, 'OLÁ') ||
+            Bot.msgContains(messagetext, 'OLA')) {
+            response = `Olá, como posso ajudar?`;
+        }
+        if(Bot.msgContains(messagetext, 'BOM DIA')) {
+            response = `Bom dia, como posso ajudar?`;
+        }
+        if(Bot.msgContains(messagetext, 'BOA TARDE')) {
+            response = `Boa tarde, como posso ajudar?`;
+        }
+        if(Bot.msgContains(messagetext, 'BOA NOITE')) {
+            response = `Boa noite, como posso ajudar?`;
+        }
+        if(Bot.msgContains(messagetext, 'OBRIGAD')) {
+            response = `Nada, eu que agradeço!`;
+        }
+        if((Bot.msgContains(messagetext, 'TUDO')
+            && Bot.msgContains(messagetext, 'TD'))
+            && (Bot.msgContains(messagetext, 'VC?')
+            || Bot.msgContains(messagetext, 'VOCE?')
+            || Bot.msgContains(messagetext, 'VOCÊ?')
+            || Bot.msgContains(messagetext, 'TU?')
+            || Bot.msgContains(messagetext, 'VAI?')
+            || Bot.msgContains(messagetext, 'ESTA?')
+            || Bot.msgContains(messagetext, 'CONTIGO?'))) {
+            response = `Estou bem, obrigada por perguntar, como posso ajudar?`;
+        }
+        if((Bot.msgContains(messagetext, 'TUDO')
+            && Bot.msgContains(messagetext, 'TD'))
+            && !(Bot.msgContains(messagetext, 'VC?')
+            || Bot.msgContains(messagetext, 'VOCE?')
+            || Bot.msgContains(messagetext, 'VOCÊ?')
+            || Bot.msgContains(messagetext, 'TU?')
+            || Bot.msgContains(messagetext, 'VAI?')
+            || Bot.msgContains(messagetext, 'ESTA?')
+            || Bot.msgContains(messagetext, 'CONTIGO?'))) {
+            response = `Posso ajudar de alguma forma?`;
+        }
+        return response;
+    }
+
+    static intencaoDuvida(datares, chat_id, messagetext) {
+        console.log('intencaoDuvida');
+        let response = '';
+
+        if(Bot.isDefault(datares)) {
+            console.log('isDefault: true');
+            if(Bot.msgContains(messagetext, 'CHECKIN') ||
+                Bot.msgContains(messagetext, 'CHECK-IN')) {
+                if(Bot.msgContains(messagetext, 'FAZER') ||
+                    Bot.msgContains(messagetext, 'EFETUAR') ||
+                    Bot.msgContains(messagetext, 'FAÇO') ||
+                    Bot.msgContains(messagetext, 'FAZ')) {
+                    response = 'O parceiro faz o Check\\-in através do acesso dele na plataforma, e quando o check\\-in é finalizado ele fica disponível para você conferir os itens, aceitar ou negar\\. [Saiba mais](https://youtu.be/20Ff4DdA05U)';
+                }
+                if(Bot.msgContains(messagetext, 'ACEITAR') ||
+                    Bot.msgContains(messagetext, 'ACEITA') ||
+                    Bot.msgContains(messagetext, 'ACEITO') ||
+                    Bot.msgContains(messagetext, 'NEGAR') ||
+                    Bot.msgContains(messagetext, 'NEGO') ||
+                    Bot.msgContains(messagetext, 'NEGA')) {
+                    response = 'Você pode aceitar ou negar um Check\\-in, através do menu \\-> Parceiros \\-> Solicitações \\-> clique sobre o Check\\-in desejado\\. [Saiba mais](https://youtu.be/\\-mrOK7_Q8IQ)';
+                }
+                if(Bot.msgContains(messagetext, 'ETIQUETA')) {
+                    response = 'Você pode imprimir etiquetas em qualquer tipo de impressora utilizando o AppCollaborative, lembre\\-se sempre de utilizar o Google Chrome, para mais detalhes preparamos esse video que ensina como configurar e imprimir: [Etiquetas](https://youtu.be/uBjCMwXFRJI)';
+                }
+            }
+            if(Bot.msgContains(messagetext, 'CHECKOUT') ||
+                Bot.msgContains(messagetext, 'CHECK-OUT')) {
+                if(Bot.msgContains(messagetext, 'FAZER') ||
+                    Bot.msgContains(messagetext, 'EFETUAR') ||
+                    Bot.msgContains(messagetext, 'FAÇO') ||
+                    Bot.msgContains(messagetext, 'FAZ')) {
+                    response = 'O parceiro faz o Check\\-out através do acesso dele na plataforma, e quando o check\\-out é finalizado ele fica disponível para você conferir os itens, aceitar ou negar\\. [Saiba mais](https://youtu.be/3_9_ceZ5nWU)';
+                }
+                if(Bot.msgContains(messagetext, 'ACEITAR') ||
+                    Bot.msgContains(messagetext, 'ACEITA') ||
+                    Bot.msgContains(messagetext, 'ACEITO') ||
+                    Bot.msgContains(messagetext, 'NEGAR') ||
+                    Bot.msgContains(messagetext, 'NEGO') ||
+                    Bot.msgContains(messagetext, 'NEGA')) {
+                    response = 'Você pode aceitar ou negar um Check\\-out, através do menu \\-> Parceiros \\-> Solicitações \\-> clique sobre o Check\\-out desejado\\. [Saiba mais](https://youtu.be/3_9_ceZ5nWU)';
+                }
+            }
+            if(Bot.msgContains(messagetext, 'ETIQUETA')) {
+                response = 'Você pode imprimir etiquetas em qualquer tipo de impressora utilizando o AppCollaborative, lembre\\-se sempre de utilizar o Google Chrome, para mais detalhes preparamos esse video que ensina como configurar e imprimir: [Etiquetas](https://youtu.be/uBjCMwXFRJI)';
+            }
+            if(Bot.msgContains(messagetext, 'PRODUTO')) {
+                console.log('CONTAINS: PRODUTO');
+                if(Bot.msgContains(messagetext, 'CADASTRAR') ||
+                    Bot.msgContains(messagetext, 'SALVAR') ||
+                    Bot.msgContains(messagetext, 'EDITAR') ||
+                    Bot.msgContains(messagetext, 'MUDAR')
+                ) {
+                    console.log('CONTAINS: CADASTRAR,SALVAR,EDITAR,MUDAR');
+                    response = 'Você como lojista pode cadastrar um novo produto, editar e deletar direto no menu \\-> Produtos \\-> Produtos, a Marca somente consegue inserir e editar produtos através de solicitações';
+                }
+                if(Bot.msgContains(messagetext, 'DESCONTO')) {
+                    response = 'Você pode cadastrar um desconto direto no produto e assim toda vez que ele for adicionado na venda ele já entrará com o valor do desconto, esse desconto também será exibido no e\\-commerce\\.';
+                }
+                if(Bot.msgContains(messagetext, 'CUSTO')) {
+                    response = 'Tanto você quanto a marca podem cadastrar o preço de custo no produto, e depois apurar os produtos vendidos pelo preço de custo no relatório de vendas,  marcando a opção de visualização com preço de custo\\.';
+                }
+                if(Bot.msgContains(messagetext, 'MARKUP') ||
+                    Bot.msgContains(messagetext, 'COMISSAO')) {
+                    response = 'Por padrão o sistema considera o calculo de comissão com base na porcentagem informada no campo de markup no cadastro do Parceiro, você pode informar o markup em porcentagem de um produto específico, dessa forma o calculo será prioritariamente com base no cadastro do produto\\.';
+                }
+                if(Bot.msgContains(messagetext, 'FOTO')) {
+                    response = 'Você pode cadastrar uma foto por produto, dependendo se a loja utiliza o nosso módulo de E\\-commerce e dependendo do tema é possível adicionar até três fotos por produto\\.';
+                }
+            }
+            if(Bot.msgContains(messagetext, 'VENDA')) {
+                if(Bot.msgContains(messagetext, 'FAZ') ||
+                    Bot.msgContains(messagetext, 'CADASTRA') ||
+                    Bot.msgContains(messagetext, 'EFETUA') ||
+                    Bot.msgContains(messagetext, 'PAGA')) {
+                    response = 'preparamos um vídeo que vai te ajudar a entender mais sobre as vendas, segue: [Video](https://youtu.be/PMYwO0bIwgk)';
+                }
+                if(Bot.msgContains(messagetext, 'APAGA') ||
+                    Bot.msgContains(messagetext, 'DELETA')) {
+                    response = 'preparamos um vídeo que mostra como deletar uma venda, segue: [Video](https://youtu.be/YRnrEKDAhtk)';
+                }
+            }
+            if(Bot.msgContains(messagetext, 'EXTRATO') ||
+                Bot.msgContains(messagetext, 'COMISSAO')) {
+                response = 'preparamos um vídeo que vai te ajudar a entender como funciona as comissões e extratos no AppCollaborative, segue: [Video](https://youtu.be/EWLcnQnaQcc)';
+            }
+            if(Bot.msgContains(messagetext, 'TROCA') ||
+                Bot.msgContains(messagetext, 'DEVOLUCAO') ||
+                Bot.msgContains(messagetext, 'DEVOLVE')) {
+                response = 'preparamos um vídeo que vai te ajudar a entender como funciona as trocas e devoluções, segue: [Video](https://youtu.be/YSgg6DY98N0)';
+            }
+            if(Bot.msgContains(messagetext, 'ECOMMERCE') ||
+                Bot.msgContains(messagetext, 'E-COMMERCE') ||
+                Bot.msgContains(messagetext, 'DELIVERY')) {
+                //todo
+            }
+            if(Bot.msgContains(messagetext, 'FISCAL') ||
+                Bot.msgContains(messagetext, 'NOTA') ||
+                Bot.msgContains(messagetext, 'NF')) {
+
+            }
+            if(Bot.msgContains(messagetext, 'USUARIO') ||
+                Bot.msgContains(messagetext, 'FUNCIONARIO') ||
+                Bot.msgContains(messagetext, 'SOCIO') ||
+                Bot.msgContains(messagetext, 'ADMINISTRADOR') ||
+                Bot.msgContains(messagetext, 'VENDEDOR')) {
+
+            }
+            if(Bot.msgContains(messagetext, 'PARCEIRO') ||
+                Bot.msgContains(messagetext, 'MARCA') ||
+                Bot.msgContains(messagetext, 'ARTESÃO') ||
+                Bot.msgContains(messagetext, 'COLABORADOR')) {
+
+            }
+        }
+        if(Bot.isPartner(datares)) {
+
+        }
+        console.log('RESPONSE', response);
+        return Bot.sendMessageMarkdown(
+            chat_id,
+            response
+        );
+    }
+
+    static intencaoConsultaQuantidade(datares, chat_id, messagetext) {
+        console.log('intencaoConsultaQuantidade');
+        let response = 'não entendi, por favor reformule sua pergunta';
+        return Bot.sendMessage(
+            chat_id,
+            response
+        );
+    }
+
+    static intencaoConsultaIdentidade(datares, chat_id, messagetext) {
+        console.log('intencaoConsultaIdentidade');
+        let response = 'não entendi, por favor reformule sua pergunta';
+        return Bot.sendMessage(
+            chat_id,
+            response
+        );
+    }
+
+    static processConversation(req, datares) {
+        const chat_id = req.body.message.chat.id;
+        const messagetext = req.body.message.text;
+        let response = '';
+        console.log('processConversation');
+        response = Bot.respostaDireta(messagetext);
+        if(Bot.msgContains(messagetext, 'COMO')) {
+            return Bot.intencaoDuvida(datares, chat_id, messagetext);
+        }
+        if(Bot.msgContains(messagetext, 'QUANTO')
+            || Bot.msgContains(messagetext, 'QUANTOS')
+            || Bot.msgContains(messagetext, 'QUANTAS')
+            || Bot.msgContains(messagetext, 'QUANTA')) {
+            return Bot.intencaoConsultaQuantidade(datares, chat_id, messagetext);
+        }
+        if(Bot.msgContains(messagetext, 'QUAL')
+            || Bot.msgContains(messagetext, 'O QUE')) {
+            return Bot.intencaoConsultaIdentidade(datares, chat_id, messagetext);
+        }
+
+        return Bot.sendMessage(
+            chat_id,
+            response
+        );
     }
 }
