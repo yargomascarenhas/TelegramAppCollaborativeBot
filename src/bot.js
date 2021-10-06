47591711fffaf8977a2e5bbb9ef5f3742ed61075
Yargo <yargomascarenhas@gmail.com>
2021-10-06 11:03:46 -0300

    doing duvida

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,6 @@
 const Collaborative = require('../lib/collaborative');
 const Api = require('../lib/api');
+const { response } = require('express');
 
 module.exports = class Bot {
     static hook(req, res) {
@@ -212,12 +213,11 @@ module.exports = class Bot {
                     response = 'O parceiro faz o Check\\-in através do acesso dele na plataforma, e quando o check\\-in é finalizado ele fica disponível para você conferir os itens, aceitar ou negar\\. [Saiba mais](https://youtu.be/20Ff4DdA05U)';
                 }
                 if(Bot.msgContains(messagetext, 'ACEITAR') ||
-                    Bot.msgContains(messagetext, 'ACEITA') ||
-                    Bot.msgContains(messagetext, 'ACEITO') ||
+                    Bot.msgContains(messagetext, 'ACEIT') ||
                     Bot.msgContains(messagetext, 'NEGAR') ||
                     Bot.msgContains(messagetext, 'NEGO') ||
                     Bot.msgContains(messagetext, 'NEGA')) {
-                    response = 'Você pode aceitar ou negar um Check\\-in, através do menu \\-> Parceiros \\-> Solicitações \\-> clique sobre o Check\\-in desejado\\. [Saiba mais](https://youtu.be/\\-mrOK7_Q8IQ)';
+                    response = 'Você pode aceitar ou negar um Check\\-in, através do menu \\-\\> Parceiros \\-\\> Solicitações \\-\\> clique sobre o Check\\-in desejado\\. [Saiba mais](https://youtu.be/\\-mrOK7_Q8IQ)';
                 }
                 if(Bot.msgContains(messagetext, 'ETIQUETA')) {
                     response = 'Você pode imprimir etiquetas em qualquer tipo de impressora utilizando o AppCollaborative, lembre\\-se sempre de utilizar o Google Chrome, para mais detalhes preparamos esse video que ensina como configurar e imprimir: [Etiquetas](https://youtu.be/uBjCMwXFRJI)';
@@ -237,7 +237,7 @@ module.exports = class Bot {
                     Bot.msgContains(messagetext, 'NEGAR') ||
                     Bot.msgContains(messagetext, 'NEGO') ||
                     Bot.msgContains(messagetext, 'NEGA')) {
-                    response = 'Você pode aceitar ou negar um Check\\-out, através do menu \\-> Parceiros \\-> Solicitações \\-> clique sobre o Check\\-out desejado\\. [Saiba mais](https://youtu.be/3_9_ceZ5nWU)';
+                    response = 'Você pode aceitar ou negar um Check\\-out, através do menu \\-\\> Parceiros \\-\\> Solicitações \\-\\> clique sobre o Check\\-out desejado\\. [Saiba mais](https://youtu.be/3_9_ceZ5nWU)';
                 }
             }
             if(Bot.msgContains(messagetext, 'ETIQUETA')) {
@@ -245,13 +245,12 @@ module.exports = class Bot {
             }
             if(Bot.msgContains(messagetext, 'PRODUTO')) {
                 console.log('CONTAINS: PRODUTO');
-                if(Bot.msgContains(messagetext, 'CADASTRAR') ||
-                    Bot.msgContains(messagetext, 'SALVAR') ||
-                    Bot.msgContains(messagetext, 'EDITAR') ||
-                    Bot.msgContains(messagetext, 'MUDAR')
+                if(Bot.msgContains(messagetext, 'CADASTR') ||
+                    Bot.msgContains(messagetext, 'SALV') ||
+                    Bot.msgContains(messagetext, 'EDIT') ||
+                    Bot.msgContains(messagetext, 'MUD')
                 ) {
-                    console.log('CONTAINS: CADASTRAR,SALVAR,EDITAR,MUDAR');
-                    response = 'Você como lojista pode cadastrar um novo produto, editar e deletar direto no menu \\-> Produtos \\-> Produtos, a Marca somente consegue inserir e editar produtos através de solicitações';
+                    response = 'Você como lojista pode cadastrar um novo produto, editar e deletar direto no menu \\-\\> Produtos \\-\\> Produtos, a Marca somente consegue inserir e editar produtos através de solicitações';
                 }
                 if(Bot.msgContains(messagetext, 'DESCONTO')) {
                     response = 'Você pode cadastrar um desconto direto no produto e assim toda vez que ele for adicionado na venda ele já entrará com o valor do desconto, esse desconto também será exibido no e\\-commerce\\.';
@@ -269,7 +268,8 @@ module.exports = class Bot {
             }
             if(Bot.msgContains(messagetext, 'VENDA')) {
                 if(Bot.msgContains(messagetext, 'FAZ') ||
-                    Bot.msgContains(messagetext, 'CADASTRA') ||
+                    Bot.msgContains(messagetext, 'CADASTR') ||
+                    Bot.msgContains(messagetext, 'FAÇO') ||
                     Bot.msgContains(messagetext, 'EFETUA') ||
                     Bot.msgContains(messagetext, 'PAGA')) {
                     response = 'preparamos um vídeo que vai te ajudar a entender mais sobre as vendas, segue: [Video](https://youtu.be/PMYwO0bIwgk)';
@@ -290,8 +290,31 @@ module.exports = class Bot {
             }
             if(Bot.msgContains(messagetext, 'ECOMMERCE') ||
                 Bot.msgContains(messagetext, 'E-COMMERCE') ||
+                Bot.msgContains(messagetext, 'SITE') ||
                 Bot.msgContains(messagetext, 'DELIVERY')) {
-                //todo
+                if(Bot.msgContains(messagetext, 'CRIA') ||
+                    Bot.msgContains(messagetext, 'CRIO') ||
+                    Bot.msgContains(messagetext, 'FAZ') ||
+                    Bot.msgContains(messagetext, 'FAÇ')) {
+                    response = 'para criar o e\\-commerce no AppCollaborative é necessário ativar o Módulo do Delivery, na sessão de módulos que fica no botão com 9 pontos no canto direito superior da tela\\.';
+                }
+                if(Bot.msgContains(messagetext, 'PAGAMENTO') ||
+                    Bot.msgContains(messagetext, 'PAGSEGURO') ||
+                    Bot.msgContains(messagetext, 'PAGAR') ||
+                    Bot.msgContains(messagetext, 'RECEB')) {
+                    response = 'no e\\-commerce do AppCollaborative é possível receber pagamentos de forma integrada com o PagSeguro e com o Pagar\\.me';
+                }
+                if(Bot.msgContains(messagetext, 'ENTREGA') ||
+                    Bot.msgContains(messagetext, 'ENVIO') ||
+                    Bot.msgContains(messagetext, 'ENVIA') ||
+                    Bot.msgContains(messagetext, 'MELHORENVIO') ||
+                    Bot.msgContains(messagetext, 'MELHOR ENVIO') ||
+                    Bot.msgContains(messagetext, 'SEDEX') ||
+                    Bot.msgContains(messagetext, 'PAC') ||
+                    Bot.msgContains(messagetext, 'JADLOG') ||
+                    Bot.msgContains(messagetext, 'CORREIO')) {
+                    response = 'o e\\-commerce do AppCollaborative possui integração com os Correios e com o Melhor Envio para cotação de fretes, com o Melhor Envio ainda é possível gerar a etiqueta direto do pedido.';
+                }
             }
             if(Bot.msgContains(messagetext, 'FISCAL') ||
                 Bot.msgContains(messagetext, 'NOTA') ||
@@ -309,14 +332,114 @@ module.exports = class Bot {
                 Bot.msgContains(messagetext, 'MARCA') ||
                 Bot.msgContains(messagetext, 'ARTESÃO') ||
                 Bot.msgContains(messagetext, 'COLABORADOR')) {
-
+                if(Bot.msgContains(messagetext, 'CADASTR') ||
+                    Bot.msgContains(messagetext, 'SALV') ||
+                    Bot.msgContains(messagetext, 'EDIT') ||
+                    Bot.msgContains(messagetext, 'CRI') ||
+                    Bot.msgContains(messagetext, 'MUD')
+                ) {
+                    response = 'Você como lojista pode cadastrar um novo parceiro, editar e deletar direto no menu \\-\\> Parceiros \\-\\> Parceiros';
+                }
             }
         }
         if(Bot.isPartner(datares)) {
-
+            if(Bot.msgContains(messagetext, 'CHECKIN') ||
+                Bot.msgContains(messagetext, 'CHECK-IN')) {
+                if(Bot.msgContains(messagetext, 'FAZER') ||
+                    Bot.msgContains(messagetext, 'EFETUAR') ||
+                    Bot.msgContains(messagetext, 'FAÇO') ||
+                    Bot.msgContains(messagetext, 'FAZ')) {
+                    response = 'Você pode fazer o Check\\-in através do seu acesso na plataforma, e quando o check\\-in é finalizado ele fica disponível para o lojista você conferir os itens, aceitar ou negar\\. [Saiba mais](https://youtu.be/20Ff4DdA05U)';
+                }
+                if(Bot.msgContains(messagetext, 'ACEITAR') ||
+                    Bot.msgContains(messagetext, 'ACEIT') ||
+                    Bot.msgContains(messagetext, 'NEGAR') ||
+                    Bot.msgContains(messagetext, 'NEGO') ||
+                    Bot.msgContains(messagetext, 'NEGA')) {
+                    response = 'O lojista pode aceitar ou negar um Check\\-in, através do menu \\-\\> Parceiros \\-\\> Solicitações \\-\\> clique sobre o Check\\-in desejado\\. [Saiba mais](https://youtu.be/\\-mrOK7_Q8IQ)';
+                }
+                if(Bot.msgContains(messagetext, 'ETIQUETA')) {
+                    response = 'Você pode imprimir etiquetas em qualquer tipo de impressora utilizando o AppCollaborative, lembre\\-se sempre de utilizar o Google Chrome, para mais detalhes preparamos esse video que ensina como configurar e imprimir: [Etiquetas](https://youtu.be/uBjCMwXFRJI)';
+                }
+            }
+            if(Bot.msgContains(messagetext, 'CHECKOUT') ||
+                Bot.msgContains(messagetext, 'CHECK-OUT')) {
+                if(Bot.msgContains(messagetext, 'FAZER') ||
+                    Bot.msgContains(messagetext, 'EFETUAR') ||
+                    Bot.msgContains(messagetext, 'FAÇO') ||
+                    Bot.msgContains(messagetext, 'FAZ')) {
+                    response = 'Você pode fazer o Check\\-out através do seu acesso dele na plataforma, e quando o check\\-out é finalizado ele fica disponível para você conferir os itens, aceitar ou negar\\. [Saiba mais](https://youtu.be/3_9_ceZ5nWU)';
+                }
+                if(Bot.msgContains(messagetext, 'ACEITAR') ||
+                    Bot.msgContains(messagetext, 'ACEITA') ||
+                    Bot.msgContains(messagetext, 'ACEITO') ||
+                    Bot.msgContains(messagetext, 'NEGAR') ||
+                    Bot.msgContains(messagetext, 'NEGO') ||
+                    Bot.msgContains(messagetext, 'NEGA')) {
+                    response = 'O lojista pode aceitar ou negar um Check\\-out, através do menu \\-\\> Parceiros \\-\\> Solicitações \\-\\> clique sobre o Check\\-out desejado\\. [Saiba mais](https://youtu.be/3_9_ceZ5nWU)';
+                }
+            }
+            if(Bot.msgContains(messagetext, 'ETIQUETA')) {
+                response = 'Você pode imprimir etiquetas em qualquer tipo de impressora utilizando o AppCollaborative, lembre\\-se sempre de utilizar o Google Chrome, para mais detalhes preparamos esse video que ensina como configurar e imprimir: [Etiquetas](https://youtu.be/uBjCMwXFRJI)';
+            }
+            if(Bot.msgContains(messagetext, 'PRODUTO')) {
+                console.log('CONTAINS: PRODUTO');
+                if(Bot.msgContains(messagetext, 'CADASTR') ||
+                    Bot.msgContains(messagetext, 'SALV') ||
+                    Bot.msgContains(messagetext, 'EDIT') ||
+                    Bot.msgContains(messagetext, 'MUD')
+                ) {
+                    response = 'Você somente consegue inserir e editar produtos através de solicitações de Check\\-in ou Check\\-out';
+                }
+                if(Bot.msgContains(messagetext, 'DESCONTO')) {
+                    response = 'Você pode cadastrar um desconto direto no produto pelo Check\\-in ou editando solicitando alteração no estoque e assim toda vez que ele for adicionado na venda ele já entrará com o valor do desconto, esse desconto também será exibido no e\\-commerce\\.';
+                }
+                if(Bot.msgContains(messagetext, 'CUSTO')) {
+                    response = 'você pode cadastrar o preço de custo no produto, e depois apurar os produtos vendidos pelo preço de custo no relatório de vendas,  marcando a opção de visualização com preço de custo\\.';
+                }
+                if(Bot.msgContains(messagetext, 'MARKUP') ||
+                    Bot.msgContains(messagetext, 'COMISSAO')) {
+                    response = 'Por padrão o sistema considera o calculo de comissão com base na porcentagem informada no campo de markup no cadastro do Parceiro, você pode informar o markup em porcentagem de um produto específico, dessa forma o calculo será prioritariamente com base no cadastro do produto\\.';
+                }
+                if(Bot.msgContains(messagetext, 'FOTO')) {
+                    response = 'Você pode cadastrar uma foto por produto, dependendo se a loja utiliza o nosso módulo de E\\-commerce e dependendo do tema é possível adicionar até três fotos por produto\\.';
+                }
+            }
+            if(Bot.msgContains(messagetext, 'VENDA')) {
+                if(Bot.msgContains(messagetext, 'FAZ') ||
+                    Bot.msgContains(messagetext, 'CADASTR') ||
+                    Bot.msgContains(messagetext, 'FAÇO') ||
+                    Bot.msgContains(messagetext, 'EFETUA') ||
+                    Bot.msgContains(messagetext, 'PAGA')) {
+                    response = 'preparamos um vídeo que vai te ajudar a entender mais sobre as vendas, segue: [Video](https://youtu.be/PMYwO0bIwgk)';
+                }
+                if(Bot.msgContains(messagetext, 'APAGA') ||
+                    Bot.msgContains(messagetext, 'DELETA')) {
+                    response = 'preparamos um vídeo que mostra como deletar uma venda, segue: [Video](https://youtu.be/YRnrEKDAhtk)';
+                }
+            }
         }
+
+        if(response === '') return Bot.sendDuvidaNaoSei(chat_id);
         console.log('RESPONSE', response);
         return Bot.sendMessageMarkdown(
+            chat_id,
+            response
+        );
+    }
+
+    static sendDuvidaNaoSei(chat_id) {
+        let responsearr = [
+            'Olha, eu não entendi, você poderia reformular sua duvida?',
+            'eu ainda estou aprendendo, então no momento eu não sei responder sua duvida',
+            'eu não sei, mas em breve vou aprender mais e estarei pronta pra responder sobre o assunto',
+            'se eu te falar que eu não sei ainda, mas vou procurar saber para aprender mais',
+            'eu não entendi, ainda to aprendendo sobre o assunto',
+            'hmmm...  eu não sei te responder sobre isso, mas vou procurar saber.'
+        ];
+        let randomIndex = Math.floor(Math.random() * (responsearr.length - 1));
+        let response = (responsearr[randomIndex]) ? responsearr[randomIndex] : responsearr[0];
+        return Bot.sendMessage(
             chat_id,
             response
         );
